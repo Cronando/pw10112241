10450ab9c1c90b77e41f7874f5446aa8607a27ff
Luis Orlando <lurear11@hotmail.com>
2024-05-15 23:59:15 -0700

    Added new VUE Project

---
+++ unidad3/PROJECT-TIENDA/frontend/tienda/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import MenuView from './views/MenuView.vue';
+</script>
+
+<template>
+  <header>
+    <MenuView></MenuView>
+  </header>
+  <RouterView/>
+    
+</template>
---
+++ unidad3/PROJECT-TIENDA/frontend/tienda/src/views/ClientesView.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>Clientes
+                    <RouterLink to="/clientes/create" class="btn btn-primary float-end">
+                    Agregar
+                    </RouterLink>
+                </h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            Nombre 
+                        </th>
+                        <th>
+                            Apellido
+                        </th>
+                        <th>
+                            Direccion
+                        </th>
+                        <th>
+                            Telefono
+                        </th>
+                        <th>
+                            RFC
+                        </th>
+                        <th>
+                            CURP
+                        </th>
+                        <th>
+                            CP
+                        </th>
+                        <th>
+                            Acciones
+                        </th>
+                    </thead>
+                        <tbody v-if="clientes.length > 0">
+                            <tr v-for="(cliente,index) in clientes" :key="index">
+                                <td>{{ cliente.id  }}</td>
+                                <td>{{ cliente.nombre  }}</td>
+                                <td>{{ cliente.apellido  }}</td>
+                                <td>{{ cliente.direccion  }}</td>
+                                <td>{{ cliente.telefono  }}</td>
+                                <td>{{ cliente.rfc  }}</td>
+                                <td>{{ cliente.curp  }}</td>
+                                <td>{{ cliente.cp  }}</td>
+                                <td>Editar &nbsp; 
+                                    <button class="btn btn-danger" @click="deleteCliente(cliente.id)">
+                                        Borrar
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody v-else>
+                            <tr>
+                                <td colspan="9" style="text-align: center;"> Sin Clientes</td>
+                            </tr>
+                        </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
