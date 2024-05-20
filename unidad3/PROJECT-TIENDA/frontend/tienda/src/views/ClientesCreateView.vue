13300bb54d63bd608aa890c5061f1222803a5e35
Cronando <lurear11@hotmail.com>
2024-05-20 11:03:39 -0700

    Update - Project now with vee-validate

--- unidad3/PROJECT-TIENDA/frontend/tienda/src/views/ClientesCreateView.vue
+++ unidad3/PROJECT-TIENDA/frontend/tienda/src/views/ClientesCreateView.vue
@@ -8,34 +8,52 @@
                 </div>
             </div>
             <div class="card-body">
-                <div class="md-3">
-                    Id <input class="form-control" type="text" v-model="model.cliente.id">
-                </div>
-                <div class="md-3">
-                    Nombre <input class="form-control" type="text" v-model="model.cliente.nombre">
-                </div>
-                <div class="md-3">
-                    Apellido <input class="form-control" type="text" v-model="model.cliente.apellido">
-                </div>
-                <div class="md-3">
-                    Direccion <input class="form-control" type="text" v-model="model.cliente.direccion">
-                </div>
-                <div class="md-3">
-                    Telefono <input class="form-control" type="text" v-model="model.cliente.telefono">
-                </div>
-                <div class="md-3">
-                    Rfc <input class="form-control" type="text" v-model="model.cliente.rfc">
-                </div>
-                <div class="md-3">
-                    Curp <input class="form-control" type="text" v-model="model.cliente.curp">
-                </div>
-                <div class="md-3">
-                    Cp <input class="form-control" type="text" v-model="model.cliente.cp">
-                </div>
-                <p></p>
-                <div class="md-3">
-                    <button class="btn btn-primary btn-block btn-lg" @click="guardarCliente()">Guardar</button>
-                </div>
+                <Form :validation-schema="validationSchema" @submit="onTodoBien">
+                    <div class="md-3">
+                        ID 
+                        <Field name="id" id="id" class="form-control" type="text" v-model="model.cliente.id" />
+                        <ErrorMessage name="id" />
+                    </div>
+                    <div class="md-3">
+                        Nombre 
+                        <Field name="nombre" id="nombre" class="form-control" type="text" v-model="model.cliente.nombre" />
+                        <ErrorMessage name="nombre" />
+                    </div>
+                    <div class="md-3">
+                        Apellidos 
+                        <Field name="apellido" id="apellido" class="form-control" type="text" v-model="model.cliente.apellido" />
+                        <ErrorMessage name="apellido" />
+                    </div>
+                    <div class="md-3">
+                        Direccion 
+                        <Field name="direccion" id="direccion" class="form-control" type="text" v-model="model.cliente.direccion" />
+                        <ErrorMessage name="direccion" />
+                    </div>
+                    <div class="md-3">
+                        Telefono 
+                        <Field name="delefono" id="telefono" class="form-control" type="text" v-model="model.cliente.telefono" />
+                        <ErrorMessage name="telefono" />
+                    </div>
+                    <div class="md-3">
+                        RFC 
+                        <Field name="rfc" id="rfc" class="form-control" type="text" v-model="model.cliente.rfc" />
+                        <ErrorMessage name="rfc" />
+                    </div>
+                    <div class="md-3">
+                        CURP 
+                        <Field name="curp" id="curp" class="form-control" type="text" v-model="model.cliente.curp" />
+                        <ErrorMessage name="curp" />
+                    </div>
+                    <div class="md-3">
+                        CP
+                        <Field name="cp" id="cp" class="form-control" type="text" v-model="model.cliente.cp" />
+                        <ErrorMessage name="cp" />
+                    </div>
+                    <p></p>
+                    <div class="md-3">
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </div>
+                </Form>              
             </div>
         </div>
 
@@ -46,11 +64,28 @@
 
 <script>
 import axios from 'axios';
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as zod from 'zod';
 
 export default {
     name: "ClientesCreate",
+    components: { Field, Form, ErrorMessage},
     data() {
+        const validationSchema = toTypedSchema(
+            zod.object({
+                id: zod.string({message: 'Campo Requerido'}).min(1) ,
+                nombre: zod.string({message: 'Campo Requerido'}).min(1) ,
+                apellido: zod.string({message: 'Campo Requerido'}).min(1) ,
+                direccion: zod.string({message: 'Campo Requerido'}).min(1) ,
+                telefono: zod.string({message: 'Campo Requerido'}).min(1) ,
+                rfc: zod.string({message: 'Campo Requerido'}).min(1) ,
+                curp: zod.string({message: 'Campo Requerido'}).min(1) ,
+                cp: zod.string({message: 'Campo Requerido'}).min(1) ,
+            })
+        )
         return {
+            validationSchema,
             mensaje: 0,
             model: {
                 cliente: {
@@ -67,8 +102,12 @@ export default {
         }
     },
     methods: {
+        onTodoBien(){
+          alert('Todo validado!');  
+        },
         guardarCliente() {
-            axios.post('http://localhost:3000/api/clientes', this.model.cliente).then(res => {
+            axios.post('http://localhost:3000/api/clientes', 
+            this.model.cliente).then(res => {
                 if (res.data.affectedRows == 1) {// si inesetamos 1 registros
                     //limpiar el cuadro de textos
                     this.model.cliente = {
